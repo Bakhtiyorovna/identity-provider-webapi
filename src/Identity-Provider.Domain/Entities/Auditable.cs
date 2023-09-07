34c010f7010bf0ba378f0efbf654ccc677ed7388
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-09-07 13:25:44 +0500

    The database has been changed

---
+++ src/Identity-Provider.Domain/Entities/Auditable.cs
@@ -0,0 +1,9 @@
+﻿using GreenSale.Domain.Entites;
+
+namespace Identity_Provider.Domain.Entities;
+
+public class Auditable: BaseEntity   
+{
+    public DateTime CreatedAt  { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ src/Identity-Provider.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace GreenSale.Domain.Entites;
+
+public class BaseEntity
+{
+    public long Id { get; set; }
+}
